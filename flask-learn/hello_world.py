cbd82ff5bbc8f7e7387d9a747b0109f479122069
Siddharth1698 <siddharth.m98@gmail.com>
2020-05-23 13:44:09 +0530

    puppy routing

--- flask-learn/hello_world.py
+++ flask-learn/hello_world.py
@@ -9,6 +9,26 @@ def index():
 def info():
     return '<h1>hello this is an info page</h1>'
 
+@app.route('/puppy/<name>')
+def puppy(name):
+    return "<h1> this is {} profile page</h1>".format(name.upper())
+
+@app.route('/puppy_latin/<name>')
+def puppy_latin(name):
+    if name[-1] == 'y':
+        name = name[:-1]
+        name = name + "iful"
+        return "<h1> this is {} profile page</h1>".format(name.upper())
+
+        # removing y from lasy character and appending it with iful.
+
+    else:
+        name = name + 'y'
+        return "<h1> this is {} profile page</h1>".format(name.upper())
+
+        # adding y to end character.
+
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
