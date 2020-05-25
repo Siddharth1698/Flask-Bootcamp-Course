b0d69c7d5ae5a356ee00dee04916b897644fb688
Siddharth1698 <siddharth.m98@gmail.com>
2020-05-26 03:47:15 +0530

    templates variables

--- flask_app/basics.py
+++ flask_app/basics.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('basics.html')
+    pokemon = "blastatoise"
+    letters = list(pokemon)
+    dict = {'water':'bulba', 'fire':'charmandar'}
+    return render_template('basics.html',name=pokemon,letters=letters,dict=dict)
 
 if __name__ == '__main__':
     app.run(debug=True)
